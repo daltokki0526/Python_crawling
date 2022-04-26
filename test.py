b898b7020520d0fb27265392600c1ad8f4468869
daltokki <102396106+daltokki0526@users.noreply.github.com>
2022-04-26 15:33:03 +0900

    20220426 test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-#안쓰는 함수 빼놓자
 
 from selenium import webdriver #셀레리움 크롤러
 from webdriver_manager.chrome import ChromeDriverManager #크롬브라우저 연결
@@ -10,8 +9,9 @@ import urllib.request
 import pyperclip #싸이트 방어 피하기
 import time #시간제어
 from selenium.webdriver.common.keys import Keys #버튼 컨트롤
+from time import sleep
 
-url = "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=289083771"
+url = "https://www.google.com/"
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get(url)
@@ -19,14 +19,18 @@ head = {'User-Agent' : "Magic Browser"}
 request = urllib.request.Request(url, headers = head) 
 response = urllib.request.urlopen(request)     
 bsObject = BeautifulSoup(driver.page_source, 'html.parser')
-title = bsObject.find("a", {"class":"Ere_bo_title"}).getText()
-mans = bsObject.find("li", {"class":"Ere_sub2_title"}).getText()
-basic = bsObject.find("div", {"class":"conts_info_list1"}).getText()
-bunru = bsObject.find("ul", {"id":"ulCategory"}).getText()
-summary = driver.find_element_by_xpath('//*[@id="div_TOC_Short"]')
-
-# print(title, mans, basic, bunru, test, summary)
-
-print(summary)
 
 
+search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+
+search_box.send_keys('greeksharifa.github.io')
+search_box.send_keys(Keys.RETURN)
+
+elements = driver.find_elements_by_xpath('//*[@id="rso"]/div[*]/div/div[1]/a/h3/span')
+
+for element in elements:
+    print(element.text)
+    print(element.text, file=open('gorio.txt', 'w', encoding='utf-8'))
+
+sleep(3)
+driver.close()
