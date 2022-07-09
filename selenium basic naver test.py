f94d21ea52a7b13b10dde7c5688f45483e7b6f16
daltokki <102396106+daltokki0526@users.noreply.github.com>
2022-07-10 01:36:29 +0900

    Update selenium basic naver test.py

--- selenium basic naver test.py
+++ selenium basic naver test.py
@@ -1,11 +1,22 @@
 from selenium import webdriver #셀레리움 크롤러
-from webdriver_manager.chrome import ChromeDriverManager #크롬브라우저 연결
+# from webdriver_manager.chrome import ChromeDriverManager #크롬브라우저 연결
 from selenium.webdriver.common.keys import Keys #버튼 컨트롤
 from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+import webbrowser
 
 url = "http://www.naver.com"
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(url)
+try:
+    path = chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+except FileNotFoundError as err:
+    print("크롬 브라우저를 찾을 수 없습니다. 설치 후 재시도 하시기 바랍니다.")
+    webbrowser.open("https://www.google.com/intl/ko/chrome/")
+
+
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.get(url)
 
 driver.find_element(By.ID, 'query').send_keys("이현세", Keys.ENTER)
 
